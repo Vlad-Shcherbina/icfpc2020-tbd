6675837fa3eeabb325bdec3f23f5be21d018551d
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-16 15:39:40 +0300

    Example submission

---
+++ src/bin/example_submission.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    assert_eq!(args.len(), 3);
+    let server_url = &args[1];
+    let player_key = &args[2];
+    println!("Server URL: {}", server_url);
+    println!("Player key: {}", player_key);
+    let resp = ureq::post(&server_url).send_string(player_key);
+    assert!(resp.ok(), "{:?}", resp);
+    println!("Response: {:?}", resp);
+    println!("Headers:");
+    for h in resp.headers_names() {
+        println!("  {}: {:?}", h, resp.header(&h).unwrap());
+    }
+    let body = resp.into_string().unwrap();
+    println!("Body: {:?}", body);
+}
