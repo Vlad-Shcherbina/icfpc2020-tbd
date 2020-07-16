4310e07480a51483d5a028a2a8c1eea905f79434
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-17 00:39:14 +0300

    data/ dir and project_path() utility

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+use std::path::{Path, PathBuf};
+
 pub fn hello() {
     println!("hello");
+}
+
+fn project_root() -> PathBuf {
+    let exe = std::fs::canonicalize(std::env::args().next().unwrap()).unwrap();
+    let mut path: &Path = &exe;
+    while path.file_name().unwrap() != "icfpc2020-tbd" {
+        path = path.parent().unwrap();
+    }
+    path.to_owned()
+}
+
+pub fn project_path(rel: impl AsRef<Path>) -> PathBuf {
+    project_root().join(rel)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_project_path() {
+        assert!(project_path("Cargo.lock").exists());
+    }
 }
