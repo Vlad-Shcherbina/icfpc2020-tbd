fcf61bed8944d7b1a8a0828f37dc5c579324a7cb
Fj <fj.mail@gmail.com>
2020-07-18 10:24:45 +0300

    bring back the simplest main

--- scratches/fj/src/bin/fj_test.rs
+++ scratches/fj/src/bin/fj_test.rs
@@ -1,5 +1,18 @@
+use tbd::ufolang::Protocol;
+use tbd::{squiggle::Data, ufolang::{eval_multidraw}, project_path, png_files::matrices_to_png};
+
+fn main1() {
+    let protocol = Protocol::load_galaxy();
+
+    let result = protocol.interact(Data::Nil, Data::make_cons(0, 0));
+    dbg!(&result.final_state);
+    dbg!(&result.data_out_to_multipledraw);
+    let matrices = eval_multidraw(result.data_out_to_multipledraw);
+    matrices_to_png(&matrices, project_path("outputs/galaxyFJ"));
+}
+
 fn main() {
-    tbd::hello();
-    let x = "asd";
-    println!("new line 15 {}", x);
+    std::thread::Builder::new().stack_size(100 * 1024 * 1024)
+        .spawn(main1).unwrap()
+        .join().unwrap();
 }
--- src/bin/ap_interact_galaxy.rs
+++ src/bin/ap_interact_galaxy.rs
@@ -2,6 +2,16 @@ use tbd::ufolang::Protocol;
 use tbd::{squiggle::Data, ufolang::{eval_multidraw}, project_path, png_files::matrices_to_png};
 use std::io::BufRead;
 
+#[allow(unused)]
+fn main_simple() {
+    let protocol = Protocol::load_galaxy();
+    let result = protocol.interact(Data::Nil, Data::make_cons(0, 0));
+    dbg!(&result.final_state);
+    dbg!(&result.data_out_to_multipledraw);
+    let matrices = eval_multidraw(result.data_out_to_multipledraw);
+    matrices_to_png(&matrices, project_path("outputs/galaxyFJ"));
+}
+
 fn main1() {
     let protocol = Protocol::load_galaxy();
 
