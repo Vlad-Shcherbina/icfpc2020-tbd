60f8c2649859bd81b42280ac6f54e62916ebad52
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-18 22:57:20 +0300

    Include data/ dir in submission to allow loading galaxy there

--- src/bin/example_submission.rs
+++ src/bin/example_submission.rs
@@ -1,8 +1,13 @@
+use tbd::ufolang::Protocol;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     assert_eq!(args.len(), 3);
     let server_url = &args[1];
     let player_key = &args[2];
+
+    let _protocol = Protocol::load_galaxy();
+
     println!("Server URL: {}", server_url);
     println!("Player key: {}", player_key);
     let resp = ureq::post(&server_url).send_string(player_key);
