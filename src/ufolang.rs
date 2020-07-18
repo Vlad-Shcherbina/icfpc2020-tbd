c16970da4114f77a1cfa22fb45504bb622c1bd1a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-18 08:52:24 +0300

    Use pretty string when logging alien_send

--- src/ufolang.rs
+++ src/ufolang.rs
@@ -386,9 +386,9 @@ impl Protocol {
                 }
             }
             state = resp.new_state;
-            eprintln!("sending to aliens: {}", resp.data_out.to_string());
+            eprintln!("sending to aliens: {:?}", resp.data_out);
             data_in = aliens_send(resp.data_out);
-            eprintln!("received from aliens: {}", data_in.to_string());
+            eprintln!("received from aliens: {:?}", data_in);
         }
     }
 }
