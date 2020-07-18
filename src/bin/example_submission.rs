ed8c12653980e98b65de9218740a4ae90ebaa6fe
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-19 01:56:04 +0300

    JOIN request

--- src/bin/example_submission.rs
+++ src/bin/example_submission.rs
@@ -1,23 +1,19 @@
 use tbd::{webapi::Endpoint, ufolang::Protocol, squiggle::Data};
 
 fn main() {
-    let protocol = Protocol::load_galaxy();
-
     let args: Vec<String> = std::env::args().collect();
     assert_eq!(args.len(), 3);
     let server_url = args[1].clone();
     let player_key = &args[2];
-    println!("Server URL: {}", server_url);
-    println!("Player key: {}", player_key);
-    let endpoint = &Endpoint::SubmissionServer { url: server_url };
+    eprintln!("Server URL: {}", server_url);
+    eprintln!("Player key: {}", player_key);
+    let endpoint = Endpoint::SubmissionServer { server_url };
     let player_key: i128 = player_key.parse().unwrap();
 
-    let mut state = Data::Nil;
-
     // JOIN
-    let data_in = Data::make_list3(2, player_key, Data::Nil);
-    let result = protocol.interact(state, data_in, endpoint);
-    dbg!(&result);
+    let req = Data::make_list3(2, player_key, Data::Nil);
+    let resp = endpoint.aliens_send(req);
+    dbg!(resp);
 
     // let resp = ureq::post(&server_url).send_string(player_key);
     // assert!(resp.ok(), "{:?}", resp);
--- src/webapi.rs
+++ src/webapi.rs
@@ -2,13 +2,13 @@
 use crate::squiggle::*;
 use crate::API_KEY;
 
-const API_PROXY: &str = "https://icfpc2020-api.testkontur.ru/aliens/send";
+const API_PROXY: &str = "https://icfpc2020-api.testkontur.ru";
 
 pub enum Endpoint {
     NoComms,
     Proxy,
     SubmissionServer {
-        url: String,
+        server_url: String,
     },
 }
 
@@ -18,31 +18,31 @@ impl Endpoint {
         // convert to string
         let modulated = modulated.iter().map(|&x| x.to_string()).collect::<Vec<_>>().join("");
 
-        println!("sending {}", modulated);
+        eprintln!("sending {}", modulated);
 
         let response = match self {
             Endpoint::NoComms => panic!(),
             Endpoint::Proxy => {
-                ureq::post(API_PROXY)
+                ureq::post(&format!("{}/aliens/send", API_PROXY))
                     .query("apiKey", API_KEY)
                     .send_string(&modulated)
             }
-            Endpoint::SubmissionServer { url } => {
-                ureq::post(url)
+            Endpoint::SubmissionServer { server_url } => {
+                ureq::post(&format!("{}/aliens/send", server_url))
                     .send_string(&modulated)
             }
         };
 
         if !response.ok() {
             // TODO: if there is rate limiting, do something about it.
-            println!("got status {} {}", response.status(), response.status_text());
-            println!("is api key correct?");
+            eprintln!("got status {} {}", response.status(), response.status_text());
+            eprintln!("is api key correct?");
             panic!();
         }
 
         let response = response.into_string().expect("valid response");
 
-        println!("got response {}", response);
+        eprintln!("got response {}", response);
         let response = bytes_to_squiggle(response.as_bytes()).expect("response is 01");
         let demodulated = demodulate(response.iter());
         demodulated.expect("valid demodulate").0
