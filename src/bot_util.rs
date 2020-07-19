27b443ede3acd2cf91fb1f7684a04c8ee234a1bd
caudate_julie <kyra-mensk@yandex.ru>
2020-07-19 23:20:33 +0300

    time prediction: collide and fly away

--- src/bot_util.rs
+++ src/bot_util.rs
@@ -25,3 +25,36 @@ pub fn ships_by_role(state: &GameState, role: Role) -> impl Iterator<Item = &Shi
         .iter()
         .filter(move |&ship| ship.ship_state.role == role)
 }
+
+
+pub struct TimePrediction {
+    pub collision: Option<i128>,
+    pub fly_off: Option<i128>,
+}
+
+// predicts time to collide to the star or to leave the frame without thrust
+pub fn predict_collisions(state: &GameState, role: &Role) -> TimePrediction {
+    let ship: Vec<&Ship> = ships_by_role(state, role.clone()).collect();
+    assert!(ship.len() == 1);
+    let ship = ship[0];
+    let r = state.field.as_ref().unwrap().planet_radius;
+    let mut position = ship.ship_state.position.clone();
+    let mut velocity = ship.ship_state.velocity.clone();
+
+    let mut count = 1;
+    loop {
+        let g = get_gravity(position);
+        velocity = velocity + g;
+        position = position + velocity;
+        if position.x.abs() <= r && position.y.abs() <= r {
+            break TimePrediction { collision: Some(count), fly_off: None };
+        }
+        if position.x.abs() <= r && position.y.abs() <= r {
+            break TimePrediction { collision: None, fly_off: Some(count) };
+        }
+        count += 1;
+        if count >= 256 { 
+            break TimePrediction { collision: None, fly_off: None };
+        };
+    }
+}
