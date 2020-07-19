a835708f501bad5c55e6091fc2eff496867f6846
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-19 14:08:56 +0300

    Remove defensive shit from example_player
    Just use their pseudocode.

--- src/bin/example_player.rs
+++ src/bin/example_player.rs
@@ -6,38 +6,18 @@ use tbd::uforest::*;
 fn main() {
     let client = Client::from_submission_argv();
 
+    let gr = client.join(JoinRequest { mystery: Data::Nil });
+    dbg!(&gr);
+
+    let gr = client.start(ShipParams {
+        fuel: 1,
+        number2: 1,
+        number3: 1,
+        number4: 1,
+    });
+    dbg!(&gr);
+
     loop {
-        eprintln!("*** new game ***");
-
-        let gr = client.join(JoinRequest { mystery: Data::Nil });
-        dbg!(&gr);
-        // assert_eq!(success, 1);
-        if gr.stage == Stage::Finished {
-            eprintln!("the game ended prematurely");
-            continue;
-        }
-        assert_eq!(gr.stage, Stage::NotStarted);
-
-        let gr = client.start(ShipParams {
-            fuel: 1,
-            number2: 1,
-            number3: 1,
-            number4: 1,
-        });
-        dbg!(&gr);
-        assert_eq!(gr.stage, Stage::Started);
-
-        loop {
-            let gr = client.commands(Commands(vec![]));
-            dbg!(&gr);
-            match gr.stage {
-                Stage::NotStarted => panic!(),
-                Stage::Started => {},
-                Stage::Finished => break,
-            }
-        }
-
-        // try to send another request just to see what happens
         let gr = client.commands(Commands(vec![]));
         dbg!(&gr);
     }
