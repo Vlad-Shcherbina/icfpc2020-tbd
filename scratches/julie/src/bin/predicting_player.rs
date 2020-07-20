2b6f3f9bdc9213757c2b3253cc23ac0f0282ee40
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-07-20 03:10:52 +0300

    Rename

--- scratches/julie/src/bin/predicting_player.rs
+++ scratches/julie/src/bin/predicting_player.rs
@@ -1,4 +1,10 @@
+use tbd::runners::{run_in_submission, run_bots_local};
+use julie::predicting_ai::PredictingAi;
 
 fn main() {
-    tbd::runners::run_in_submission(julie::predicting_ai::PredictingAi {});
+    if tbd::is_running_in_submission() {
+        run_in_submission(PredictingAi {});
+    } else {
+        run_bots_local(PredictingAi {}, PredictingAi {});
+    }
 }
