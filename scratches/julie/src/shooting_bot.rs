e9169e33c622df68e85394d7895bece2f950b5ab
caudate_julie <kyra-mensk@yandex.ru>
2020-07-20 14:38:22 +0300

    rudimental shooter - correct params

--- scratches/julie/src/shooting_bot.rs
+++ scratches/julie/src/shooting_bot.rs
@@ -8,19 +8,11 @@ pub struct ShootingAI {}
 
 impl Ai for ShootingAI {
     fn initial_ship_params(&mut self, spec: &GameSpec) -> ShipParams {
-        let mut params = match spec.role {
-            Role::Defender => ShipParams {
-                fuel: 0,
-                laser: 0,
-                cooling: 30,
-                hull: 1,
-            },
-            Role::Attacker => ShipParams {
-                fuel: 0,
-                laser: 0,
-                cooling: 20,
-                hull: 1,
-            }
+        let mut params = ShipParams {
+            fuel: 0,
+            laser: 10,
+            cooling: 30,
+            hull: 1,
         };
         compute_fuel_from_params(&mut params, spec).unwrap();
         params
@@ -44,19 +36,19 @@ impl Ai for ShootingAI {
                 });
                 expected_heat += 8;
             }
+
             if ship.heat_capacity - expected_heat < 10 { continue; }
             for opponent in ships_by_role(state, other_role) {
                 if get_hp(opponent) <= 0 { continue; }
-                // println!("Shoot at {:?}", get_expected_position(opponent));
+                // println!("Turn {}, Shoot from {:?} at {:?}", state.steps, ship.position, get_expected_position(opponent));
                 commands.push(Command::Shoot {
                     ship_id: ship.ship_id,
                     target: get_expected_position(opponent),
-                    power: ship.ship_params.laser,
+                    power: 10,
                 });
                 break;
             }
-        }        
-
+        }
         Commands(commands)
     }
 }
